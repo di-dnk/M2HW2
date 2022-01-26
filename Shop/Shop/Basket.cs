e61e59fc902bc372a33bbfa8794cf9f145a7e2ee
Diana <dia010676@gmail.com>
2022-01-26 19:54:59 +0200

    Shop

---
+++ Shop/Shop/Basket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop
+{
+    public class Basket
+    {
+        public Product[] Products;
+
+        public void Add()
+        {
+            Catalog catalog = new Catalog();
+            string name;
+            List<Product> Input = new List<Product>();
+            for (int i = 0; i < 10; i++)
+            {                               
+                name = Console.ReadLine();
+                if (name == "0")
+                    break;
+                else
+                    for (int j = 0; j < catalog.Products.Length; j++)
+                    {
+                        if (catalog.Products[j].Name == name)
+                            Input.Add(catalog.Products[j]);
+                    }                                   
+            }
+            Products = Input.ToArray();
+        }                 
+    }
+}
